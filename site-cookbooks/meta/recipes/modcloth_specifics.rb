8b3bb4199a358c4f50bfce90036d1186e177b6b6
jtwb <jason.benterou+github@gmail.com>
2013-06-10 13:00:40 -0600

    Add propane to modcloth recipe

--- site-cookbooks/meta/recipes/modcloth_specifics.rb
+++ site-cookbooks/meta/recipes/modcloth_specifics.rb
@@ -1,4 +1,5 @@
 include_recipe "pivotal_workstation::redis"
 include_recipe "pivotal_workstation::bash_profile-aliases"
+include_recipe "sprout-osx-apps::propane"
 
 pivotal_workstation_bash_profile_include "modcloth"

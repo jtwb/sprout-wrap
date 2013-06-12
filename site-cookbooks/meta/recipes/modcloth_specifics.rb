daeed57ff169617a65a5518742efb098cbffbb1e
jtwb <jason.benterou+github@gmail.com>
2013-06-12 16:55:08 -0700

    Add heroku_toolbelt to ModCloth specifics

--- site-cookbooks/meta/recipes/modcloth_specifics.rb
+++ site-cookbooks/meta/recipes/modcloth_specifics.rb
@@ -1,5 +1,6 @@
 include_recipe "pivotal_workstation::redis"
 include_recipe "pivotal_workstation::bash_profile-aliases"
 include_recipe "sprout-osx-apps::propane"
+include_recipe "pivotal_workstation::heroku_toolbelt"
 
 pivotal_workstation_bash_profile_include "modcloth"
